20c8f064538e904194b7ec693b07ea19f9aab836
Semicolon-rw <Adol1329@users.noreply.github.com>
2026-08-03 10:23:28 +0200

    create simple program that user to enter any king of information

---
+++ index.py
@@ -0,0 +1,9 @@
+# I am going to create simple program that user to enter any king of information
+
+print("Simple Program")
+fullname=input("Enter full name:")
+
+age = int(input("Enter Age:"))
+address= input ("Enter Address:")
+print(f"{fullname} {age} {address}")
+
