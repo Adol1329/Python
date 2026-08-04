40ec6f8d5bf0f2e6ceb513e253cb589495cf5fc0
Semicolon-rw <Adol1329@users.noreply.github.com>
2026-08-04 10:26:47 +0200

    Create a list containing 5 student marks.

---
+++ challanges/q4.py
@@ -0,0 +1,21 @@
+"""
+Question 4: Student Scores (List)
+
+Create a list containing 5 student marks.
+
+Write a program to display:
+
+* The highest mark.
+* The lowest mark.
+* The total marks.
+* The average mark.
+
+⸻
+"""
+
+marks =[10,30,43,20.1]
+
+print("the highest mark is:", max(marks))
+print("The lowest mark is:",min(marks))
+print("The total marks is:", sum(marks))
+print("The average mark is:", sum(marks)/ len(marks))
