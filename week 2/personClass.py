46a1d4a6d52821c2c85cfc463ce24048554de1ca
Semicolon-rw <Adol1329@users.noreply.github.com>
2026-08-11 09:37:12 +0200

    still improving my Class

--- week 2/personClass.py
+++ week 2/personClass.py
@@ -1,10 +1,17 @@
 class Person:
-    def __init__(self, name):
+    def __init__(self, name, gender, age):
         self.name = name
+        self.gender=gender
+        self.age=age
+        
         
     def __str__(self):
         return self.name
+        return self.gender
+        return self.age
     
         
-p1=Person("ado")
-print(p1)
+p1=Person("ado","male","10")
+print(p1.name)
+print(p1.gender)
+print(p1.age)
