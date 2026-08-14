87f8b570a3e3da4158533fc3816acd50a7577dfa
Semicolon-rw <Adol1329@users.noreply.github.com>
2026-08-14 11:27:02 +0200

    improve and correcting errors

--- week 2/cal.py
+++ week 2/cal.py
@@ -1,26 +1,25 @@
 try:
-    num1= int(input("Enter first number: !\n"))
+    num1= int(input("Enter first number: \n"))
     num2 = int(input("Enter second number: \n"))
-    sum=num1+num2
-    sub=num1-num2
-    division=num1/num2
-    multiple=num1*num2
-    print("1 sum")
-    print("2 sub")
-    print("3 divide")
-    print("4 Mult")
-    option = int(input("Choose btn (1,2,3,4):)"))
-    match option:
-        case(1):
-            print(f"the sum of two numbers : {sum}")
+
+    operate =(input("Choose +, -,/,*"  ))
+    match operate:
+        case"+":
+            print(f"sum : {num1+num2}")
             
-        case(2):
-            print(f" the subraction of two numbers: {sub}")
+        case"-":
+            print(f"sub: {num1-num2}")
             
-        case(3):
-            print(f" the result  of two numbers: {division}")
-        case(4):
-            print(f" the Multiplication of two numbers: {multiple}")    
-            
-except:
-    print("hello")            
+        case"/":
+            if num2==0:   
+                print(f"cannot divide by zero.")
+            else:
+                print(f"Division: {num1/num2}")  
+        case("*"):
+            print(f"Multiplication: {num1*num2}")    
+        case _:
+            print("Invalid operator")                
+except ValueError:
+    print("please enter vaild whole numbers.")
+finally:
+    print("Thank you!!")                
