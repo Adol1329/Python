bdb7eb3c38c6474f61de7205a209c42d03c51349
Semicolon-rw <Adol1329@users.noreply.github.com>
2026-08-26 13:00:04 +0200

    Recap

---
+++ revision/student/myproject/lecture/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Lecture(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=254)
+    department=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return (f"{self.name}, {self.email} ,{self.department}")
